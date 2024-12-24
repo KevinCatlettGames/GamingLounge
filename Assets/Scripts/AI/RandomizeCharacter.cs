cea2556424697b26bade7ab1f2f56564fd06634b
Kevin Catlett <kevin.catlett@terovania.de>
2024-12-24 11:56:04 +0100

    Changed Main Menu to showcase NPCs

--- Assets/Scripts/AI/RandomizeCharacter.cs
+++ Assets/Scripts/AI/RandomizeCharacter.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Behavior; 
 
@@ -7,21 +9,51 @@ public class RandomizeCharacter : MonoBehaviour
     [SerializeField] private GameObject[] characterMeshes;
     [SerializeField] private int[] headIndexes;
     [SerializeField] private int[] clothIndexes;
-    [SerializeField] private BehaviorGraphAgent graphAgent; 
+    [SerializeField] private BehaviorGraphAgent graphAgent;
+    public float waitTime = 1; 
+    int maxNPCTime = 9;
+    private int currentNPCTime = 0;
+    private int alienToUse = 0; 
     private void Awake()
     {
-        foreach (GameObject character in characters)
+        StartCoroutine(NewLook());
+    }
+
+    private IEnumerator NewLook()
+    {
+        for(int i = 0; i < characters.Length;i++) 
         {
-            character.SetActive(false);
+            if(i != alienToUse)
+                characters[i].SetActive(false);
         }
         
-        int randomIndex = Random.Range(0, characters.Length);
+        characters[alienToUse].SetActive(true);
+        graphAgent.BlackboardReference.SetVariableValue("Anim", characters[alienToUse].GetComponent<Animator>());
         
-        characters[randomIndex].SetActive(true);
-        graphAgent.BlackboardReference.SetVariableValue("Anim", characters[randomIndex].GetComponent<Animator>());
+        characterMeshes[alienToUse].GetComponent<SkinnedMeshRenderer>().materials[headIndexes[alienToUse]]
+            .SetFloat("_Hue", currentNPCTime);
+        characterMeshes[alienToUse].GetComponent<SkinnedMeshRenderer>().materials[clothIndexes[alienToUse]]
+            .SetFloat("_Hue", currentNPCTime);
+        yield return new WaitForSeconds(waitTime);
+
+        currentNPCTime++;
+        if (currentNPCTime >= maxNPCTime)
+        {
+            if (alienToUse == characters.Length-1)
+            {
+                alienToUse = 0;
+            }
+            else
+            {
+                alienToUse++;
+                foreach (GameObject character in characters)
+                {
+                    character.SetActive(false);
+                }
+            }
+            currentNPCTime = 0; 
+        }
         
-        int randomColor = Random.Range(0, 9);
-        characterMeshes[randomIndex].GetComponent<SkinnedMeshRenderer>().materials[headIndexes[randomIndex]].SetFloat("_Hue", randomColor);
-        characterMeshes[randomIndex].GetComponent<SkinnedMeshRenderer>().materials[clothIndexes[randomIndex]].SetFloat("_Hue", randomColor);
+        StartCoroutine(NewLook());
     }
 }
